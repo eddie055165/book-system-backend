cac0aeca2c3dfab05a7f78d684791820f62d6ed8
eddie055165 <eddie055165@gmail.com>
2025-02-08 09:42:11 +0800

    add cors

--- src/main/java/com/booksystemback/book_system_back/controller/BorrowingRecordController.java
+++ src/main/java/com/booksystemback/book_system_back/controller/BorrowingRecordController.java
@@ -4,12 +4,10 @@ import com.booksystemback.book_system_back.service.BorrowingRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/borrowings")
 public class BorrowingRecordController {
 
--- src/main/java/com/booksystemback/book_system_back/controller/UserController.java
+++ src/main/java/com/booksystemback/book_system_back/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/users")
 public class UserController {
 
